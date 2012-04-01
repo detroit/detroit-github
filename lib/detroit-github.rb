0c62333151db5ba49b07d96ada391da65874c550
7rans <transfire@gmail.com>
2012-04-01 13:13:16 -0400

    Use new assembly interface.

--- lib/detroit-github.rb
+++ lib/detroit-github.rb
@@ -19,6 +19,9 @@ module Detroit
     PAGES_BRANCH = "gh-pages"
 
     #
+    DEFAULT_FOLDER = "pages"
+
+    #
     DEFAULT_REMOTE = "origin"
 
     #
@@ -33,13 +36,22 @@ module Detroit
     # Commit message.
     attr_accessor :message
 
+    # Pages folder to use (defaults to 'pages').
+    attr_accessor :folder
+
+    #
+    alias_accessor :gh_pages, :folder
+
+    # Use a local check out?
+    #attr_accessor :local
+
     # List of directories and files to transfer.
     # If a single directory entry is given then the contents
     # of that directory will be transfered.
-    attr_reader :sitemap
+    #attr_reader :sitemap
 
     # List of any files/directory to not overwrite in branch.
-    attr_reader :keep
+    #attr_reader :keep
 
     # Set sitemap.
     def sitemap=(entries)
@@ -64,63 +76,83 @@ module Detroit
     # The repository branch (ALWAYS "gh-pages").
     attr_reader :branch
 
+    #  A S S E M B L Y
 
-    #  A S S E M B L Y   S T A T I O N S
-
-    #
-    def station_publish
-      publish
+    # Do not need to `prepare` if gh_pages directory is already created.
+    def assemble?(station, options={})
+      case station
+      when :prepare
+        child ? false : true
+      when :publish, :clean
+        true
+      end
     end
 
-    #
-    def station_clean
-      clean
+    # Attach to `prepare`, `publish` and `clean`.
+    def assemble(station, options={})
+      case station
+      when :prepare then prepare
+      when :publish then publish
+      when :clean   then clean
+      end
     end
-
 
     #  S E R V I C E   M E T H O D S 
 
+    #
+    def prepare
+      return if child
+
+      if File.exist?(pgdir)
+        abort "Can't setup gh-pages at #{folder}. Directory already exists."
+      end
+
+      # does the master repo have a gh-pages branch?
+      new = !master.branches.find{ |b| b.name == branch }
+
+      if new
+        create_branch
+      else
+        clone_branch
+      end
+
+      update_gitignore
+    end
+
     # Publish sitemap files to branch (gh-pages).
     def publish
-      if expanded_sitemap.empty?
-        report "No files selected for publishing."
+      if !File.directory?(pgdir)
+        report "No pages folder found (#{folder})."
         return
       end
 
-      url = repo.config["remote.#{remote}.url"]
-      dir = Dir.pwd  # project.root
-      new = !repo.branches.find{ |b| b.name == branch }
-
-      create_branch if new
-
-      chdir(tmpdir) do
-        sh %[git clone --local #{dir} .]
-        sh %[git checkout #{branch}]
+      chdir(pgdir) do
+        sh %[git push #{remote} #{branch}]
       end
 
-      paths_to_remove.each do |path|
-        path = File.join(tmpdir, path)
-        rm path if File.file?(path)
-      end
+      #paths_to_remove.each do |path|
+      #  path = File.join(tmpdir, path)
+      #  rm path if File.file?(path)
+      #end
 
-      expanded_sitemap.each do |(src, dest)|
-        trace "transfer: #{src} => #{dest}"
-        if directory?(src)
-          out = File.join(tmpdir, dest)
-          mkdir_p(out) unless File.directory?(out)
-        else
-          out = File.join(tmpdir, dest)
-          mkdir_p(File.dirname(out))
-          install(src, out) unless keep.include?(dest)
-        end
-      end
+      #expanded_sitemap.each do |(src, dest)|
+      #  trace "transfer: #{src} => #{dest}"
+      #  if directory?(src)
+      #    out = File.join(tmpdir, dest)
+      #    mkdir_p(out) unless File.directory?(out)
+      #  else
+      #    out = File.join(tmpdir, dest)
+      #    mkdir_p(File.dirname(out))
+      #    install(src, out) unless keep.include?(dest)
+      #  end
+      #end
 
-      chdir(tmpdir) do
-        sh %[git add --all]
-        sh %[git commit -q -a -m "#{message}"]
-        sh %[git push #{remote} #{branch}]  # TODO: add --dry-run if trial?
-        sh %[git push #{url} #{branch}]     # TODO: add --dry-run if trial?
-      end
+      #chdir(tmpdir) do
+      #  sh %[git add --all]
+      #  sh %[git commit -q -a -m "#{message}"]
+      #  sh %[git push #{remote} #{branch}]  # TODO: add --dry-run if trial?
+      #  sh %[git push #{url} #{branch}]     # TODO: add --dry-run if trial?
+      #end
     end
 
     #
@@ -130,42 +162,60 @@ module Detroit
 
   private
 
-    # If the gh-pages branch doesn't exist we will need to create it.
-    #--
-    # TODO: This assumes we started out on master. Look up current and swtich back to that.
-    #++
-    def create_branch
-      size = repo.status.changed.size +
-             repo.status.added.size   +
-             repo.status.deleted.size
-      if size > 0
-        abort "Cannot create gh-pages branch in dirty repo."
+    # Clone the repo to a local folder, checkout the pages
+    # branch and remove the master branch.
+    #
+    # NOTE: Considered using `sh %[git clone --local . #{pgdir}]` but
+    # that appeared to require duplicate commits, once in pgdir and
+    # then in root.
+    def clone_branch
+      sh %[git clone #{url} #{pgdir}]
+      Dir.chdir(pgdir) do
+        sh %[git checkout #{branch}]
+        sh %[git branch -d master]
       end
-      ## save any outstadning changes
-      sh 'git stash save'
-      ## yes, only a (git) fanboy could possibly think this  
-      ## is a good way to handle websites
-      sh 'git symbolic-ref HEAD refs/heads/gh-pages'
-      sh 'rm .git/index'
-      sh 'git clean -fdxq'
-      sh 'echo "My GitHub Page" > index.html'
-      sh 'git add .'
-      sh 'git commit -a -m "First pages commit"'
-      sh 'git push origin gh-pages'
-      ## gh-pages is made, let's get back to master
-      sh 'git checkout master'
-      sh 'git stash pop'
     end
 
-    #--
-    # TODO: Does the POM Project provide the site directory?
-    #++
+    # TODO: This assumes we started out on master. Look up current and swtich back to that.
+
+    # If the gh-pages branch doesn't exist we need to create it.
+    def create_branch
+      sh %[git clone #{url} #{pgdir}]
+      Dir.chdir(pgdir) do
+        sh %[git symbolic-ref HEAD refs/heads/#{branch}]
+        sh %[rm .git/index]
+        sh %[git clean -fdxq]
+        sh %[echo "My GitHub Page" > index.html]
+        sh %[git add .]
+        sh %[git commit -a -m "First pages commit."]
+        sh %[git push origin #{branch}]
+        sh %[git checkout #{branch}]
+        sh %[git branch -d master]
+      end
+    end
+
+    # Only updates .gitignore if folder is not already present.
+    def update_gitignore
+      file = File.join(root, '.gitignore')
+      if File.exist?(file)
+        done = false
+        File.readlines(file).each do |line|
+          done = true if line.strip == folder
+        end
+        append(file, folder) unless done
+      else
+        write(file, folder)
+      end
+    end
+
+    # TODO: Does the POM Project provide the site folder?
+
+    #
     def initialize_defaults
       @branch   ||= PAGES_BRANCH
+      @folder   ||= DEFAULT_FOLDER
       @remote   ||= DEFAULT_REMOTE
       @message  ||= DEFAULT_MESSAGE
-      @sitemap  ||= default_sitemap
-      @keep     ||= []
     end
 
     # Require Grit.
@@ -176,8 +226,28 @@ module Detroit
     end
 
     # Get a cached Grit::Repo instance.
-    def repo
-      @repo ||= Grit::Repo.new(project ? project.root : Dir.pwd)
+    def master
+      @master ||= Grit::Repo.new(root)
+    end
+
+    # Remote URL for master.
+    def url
+      @url ||= master.config["remote.#{remote}.url"]
+    end
+
+    # Child Grit::Repo instance, i.e. a repo just for gh-pages.
+    def child
+      @child ||= \
+        begin
+          Grit::Repo.new(pgdir) if File.directory?(pgdir)
+        rescue Grit::InvalidGitRepositoryError
+          nil          
+        end
+    end
+
+    # Location of child folder.
+    def pgdir
+      @pgdir ||= File.join(root,folder)
     end
 
     # Cached system temporary directory.
@@ -189,6 +259,12 @@ module Detroit
       )
     end
 
+    #
+    def root
+      project ? project.root : Dir.pwd
+    end
+
+=begin
     # Default sitemap includes the `site` directoy, if it exists.
     # Otherwise the `doc` directory.
     def default_sitemap
@@ -232,6 +308,7 @@ module Detroit
       paths = arrival - present - keep
       paths
     end
+=end
 
   public
 
